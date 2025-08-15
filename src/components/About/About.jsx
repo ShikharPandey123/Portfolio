386224a928025ca025fce7e91bcf77bdb3a2a619
Shikhar Pandey <shikhar2003.pandey@gmail.com>
2025-08-15 18:59:31 +0530

    resume updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1OLUJGktPaC3Yd0-srxclpa4qp4zpk7Ab/view?usp=sharing"
+            href="https://drive.google.com/file/d/1mO57rVmfr3rbK1R2bZdDBj7hlpDUr62v/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
