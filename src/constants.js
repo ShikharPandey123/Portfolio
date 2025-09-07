4c4ba1b1abea741e48d2e89d95520bd31b30549c
Shikhar <shikhar2003.pandey@gmail.com>
2025-09-07 17:29:43 +0530

    changes

--- src/constants.js
+++ src/constants.js
@@ -41,13 +41,6 @@ import Indibus from "./assets/company_logo/indibus_logo.png";
 // Education Section Logo's
 import akgecLogo from "./assets/education_logo/akgec_logo.png";
 import central_academy_logo from "./assets/education_logo/Central_academy_logo.png";
-// import vpsLogo from './assets/education_logo/vps_logo.png';
-
-// Project Section Logo's
-// import githubdetLogo from './assets/work_logo/github_det.png';
-// import csprepLogo from './assets/work_logo/cs_prep.png';
-// import movierecLogo from './assets/work_logo/movie_rec.png';
-// import npmLogo from './assets/work_logo/npm.png';
 import commitly from "./assets/work_logo/commitly.png";
 import food_order from "./assets/work_logo/food_order.png";
 import streamify from "./assets/work_logo/streamify.png";
@@ -171,15 +164,6 @@ traffic of 50+ users, optimized frontend performance with Vite, Axios, and React
 ];
 
 export const education = [
-  // {
-  //   id: 0,
-  //   img: glaLogo,
-  //   school: "GLA University, Mathura",
-  //   date: "Sept 2022 - July 2024",
-  //   grade: "7.81 CGPA",
-  //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-  //   degree: "Master of Computer Applications - MCA",
-  // },
   {
     id: 0,
     img: akgecLogo,
@@ -230,57 +214,8 @@ export const projects = [
     github: "https://github.com/ShikharPandey123/github_saas.git",
     webapp: "https://commitly-ashen.vercel.app/",
   },
-  {
-    id: 1,
-    title: "Streamify",
-    description:
-      "Streamify is a real-time chat and video call web app featuring live messaging, friend suggestions, notifications, and 32 customizable UI themes. It uses React with React Router, Tailwind CSS, and DaisyUI for a responsive, modern frontend. Real-time messaging is powered by the Stream Chat SDK, supporting typing indicators, emoji reactions, and threaded messages. The backend is built with Node.js, Express, and MongoDB, with JWT-based authentication and secure cookie handling. Streamify provides a seamless social experience with robust real-time communication and dynamic, user-friendly interfaces.",
-    image: streamify,
-    tags: [
-      "React JS",
-      "React Router",
-      "React Query",
-      "Node.js",
-      "MongoDB",
-      "Express",
-      "StreamChat.io",
-    ],
-    github: "https://github.com/ShikharPandey123/Streamify.git",
-    webapp: "https://streamify-8sh2.onrender.com/",
-  },
-  {
-    id: 2,
-    title: "Job Application Portal",
-    description:
-      "This is a MERN-based job application platform where users can register, upload resumes, and apply for jobs, while admins can view and manage applications. The frontend is built with React (Vite), React Router, Axios, and React Toastify for a responsive UI. The backend uses Node.js, Express, and MongoDB, with JWT authentication, role-based access, and Multer for file uploads. RESTful API architecture ensures smooth communication between client and server. The app is deployed on Render, providing a fully functional, real-world job application system.",
-    image: job_portal,
-    tags: [
-      "React JS",
-      "React Router",
-      "Axios",
-      "React Toastify",
-      "Node.js",
-      "Express",
-      "MongoDB",
-      "JWT",
-      "Multer",
-    ],
-
-    github: "https://github.com/ShikharPandey123/Job-Application-MERN.git",
-    webapp: "https://github.com/ShikharPandey123/Job-Application-MERN.git",
-  },
-  {
-    id: 3,
-    title: "Food Order Web App",
-    description:
-      "This is a full-stack web application that allows users to browse food items, add them to a cart, and place orders online. It features user authentication and authorization, order history tracking, and a responsive interface. The frontend is built with React and Redux for state management, while the backend uses Node.js to handle API requests and data processing. The app demonstrates a complete e-commerce workflow for food ordering with secure user interactions and real-time order management.",
-    image: food_order,
-    tags: ["React JS", "Redux", "Node.js", "REST API"],
-    github: "https://github.com/ShikharPandey123/Food-Order-Web-App.git",
-    webapp: "https://food-order-web-app-pi.vercel.app",
-  },
-  {
-  id: 4,
+    {
+  id: 1,
   title: "MediCare Pro: AI-Powered Medical Management System",
   description:
     "MediCare Pro is a comprehensive AI-powered medical management system built with Next.js 15, TypeScript, Node.js, and PostgreSQL, designed to streamline healthcare workflows and enhance patient care. It features secure doctor authentication, comprehensive patient management, consultation tracking, AI-powered audio transcription using OpenAI Whisper, intelligent summarization of medical consultations, automated clinical note generation, and medical report creation. The platform includes real-time dashboards, visit management, audio processing with FFmpeg, AWS S3 file storage, and a modern responsive healthcare UI built with shadcn/ui and Radix components. With premium OpenAI integration for transcription and summarization, the system enables efficient medical documentation, enhanced patient care workflows, and intelligent healthcare insights.",
@@ -301,35 +236,54 @@ export const projects = [
   ],
   github: "https://github.com/ShikharPandey123/ai-medical-frontend.git",
   webapp: "https://saramedico.com",
-}
-  // {
-  //   id: 6,
-  //   title: "Coding Master",
-  //   description:
-  //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-  //   image: cmLogo,
-  //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-  //   github: "https://codingmasterweb.in/",
-  //   webapp: "https://codingmasterweb.in/",
-  // },
-  // {
-  //   id: 7,
-  //   title: "Image Search App",
-  //   description:
-  //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-  //   image: imagesearchLogo,
-  //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-  //   github: "https://github.com/codingmastr/Image-Search-App",
-  //   webapp: "https://imagsearch.netlify.app/",
-  // },
-  // {
-  //   id: 8,
-  //   title: "Image Background Remover",
-  //   description:
-  //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-  //   image: removebgLogo,
-  //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-  //   github: "https://github.com/codingmastr/Image-Background-Remover",
-  //   webapp: "https://removeyourbg.netlify.app/",
-  // },
+},
+  {
+    id: 2,
+    title: "Streamify",
+    description:
+      "Streamify is a real-time chat and video call web app featuring live messaging, friend suggestions, notifications, and 32 customizable UI themes. It uses React with React Router, Tailwind CSS, and DaisyUI for a responsive, modern frontend. Real-time messaging is powered by the Stream Chat SDK, supporting typing indicators, emoji reactions, and threaded messages. The backend is built with Node.js, Express, and MongoDB, with JWT-based authentication and secure cookie handling. Streamify provides a seamless social experience with robust real-time communication and dynamic, user-friendly interfaces.",
+    image: streamify,
+    tags: [
+      "React JS",
+      "React Router",
+      "React Query",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "StreamChat.io",
+    ],
+    github: "https://github.com/ShikharPandey123/Streamify.git",
+    webapp: "https://streamify-8sh2.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Job Application Portal",
+    description:
+      "This is a MERN-based job application platform where users can register, upload resumes, and apply for jobs, while admins can view and manage applications. The frontend is built with React (Vite), React Router, Axios, and React Toastify for a responsive UI. The backend uses Node.js, Express, and MongoDB, with JWT authentication, role-based access, and Multer for file uploads. RESTful API architecture ensures smooth communication between client and server. The app is deployed on Render, providing a fully functional, real-world job application system.",
+    image: job_portal,
+    tags: [
+      "React JS",
+      "React Router",
+      "Axios",
+      "React Toastify",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "JWT",
+      "Multer",
+    ],
+
+    github: "https://github.com/ShikharPandey123/Job-Application-MERN.git",
+    webapp: "https://github.com/ShikharPandey123/Job-Application-MERN.git",
+  },
+  {
+    id: 4,
+    title: "Food Order Web App",
+    description:
+      "This is a full-stack web application that allows users to browse food items, add them to a cart, and place orders online. It features user authentication and authorization, order history tracking, and a responsive interface. The frontend is built with React and Redux for state management, while the backend uses Node.js to handle API requests and data processing. The app demonstrates a complete e-commerce workflow for food ordering with secure user interactions and real-time order management.",
+    image: food_order,
+    tags: ["React JS", "Redux", "Node.js", "REST API"],
+    github: "https://github.com/ShikharPandey123/Food-Order-Web-App.git",
+    webapp: "https://food-order-web-app-pi.vercel.app",
+  },
 ];
